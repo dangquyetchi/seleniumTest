3e9de074d6fdb03828ad563ca4aa8b7a55a1b97e
Chis <dangquyetchi273@gmail.com>
2024-10-04 08:31:27 +0700

    Cập nhật cấu hình workflow cho GitHub Actions

--- src/test/java/tesssst/com/UserRegistrationTest.java
+++ src/test/java/tesssst/com/UserRegistrationTest.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest {
